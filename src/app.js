f63d207d50314e16a278bf5c6ca0b953972294c0
Shreyas <106990933+shreyxs9@users.noreply.github.com>
2025-01-25 19:49:18 +0530

    get nd post req

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
 const app =express();  
-app.use((req,res)=> {
+
+app.get("/hello",(req,res)=> {
     res.send("heloooo bkl's");
+})
+app.get("/hello/2",(req,res)=> {
+    res.send("heloooo lause");
 })
 app.listen(3000, ()=>{
     console.log("running on 3000")
