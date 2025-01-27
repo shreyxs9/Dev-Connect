7b5ca667afeb48fbbe8d53d4476c97fc8af40425
Shreyas <106990933+shreyxs9@users.noreply.github.com>
2025-01-27 19:47:33 +0530

    data validation

--- src/app.js
+++ src/app.js
@@ -7,11 +7,35 @@ const User = require("./models/user");
 app.use(express.json());
 
 app.post("/signup", async (req,res) => {
-
     const user = new User(req.body);
     await user.save();
     res.send("user saved to db");
 });
+app.get("/feed", async (req,res)=>{
+     try{
+        const users = await User.findOneAndDelete({email:"www.shryas.com"});
+        res.send(users);
+     } catch(err){ {
+        res.semd("something went wrong");
+     }
+}})
+app.get("/feed", async (req,res)=>{
+     try{
+        const users = await User.findOneAndDelete({email:"www.shryas.com"});
+        res.send(users);
+     } catch(err){ {
+        res.semd("something went wrong");
+     }
+}})
+app.patch("/feed", async (req,res)=>{
+     try{
+        const users = await User.updateMany({email:"www.shreyas.com"});
+        res.send(users);
+     } catch(err){ {
+        res.semd("something went wrong");
+     }
+}})
+
 connectDb().then(()=>{
     console.log("db connect")
     app.listen(3000, () => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,17 +1,33 @@
 const mongoose = require('mongoose');
+const validator =require('validator');
 
 const userSchema =  new mongoose.Schema({ 
      firstName: {
         type:String,
+        required: true,
+        minLength:4,
+        maxLength:20,
      },
      lastName :{
         type: String,
+        required: true,
+        minLength:4,
+        maxLength:20,
      },
      email:{
         type:String,
+        required: true,
+        unique:true, 
+        trim:true, 
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Email is invalid"+value);
+            }
+        }
      },
      password:{
         type:String,
+        required: true,
      },  
      age:{
         type:String,
