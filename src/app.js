4d6b651e6226b81aa4d763081fa14654d49edf09
Shreyas <106990933+shreyxs9@users.noreply.github.com>
2025-01-28 15:59:58 +0530

    jwt implemented

--- src/app.js
+++ src/app.js
@@ -6,9 +6,13 @@ const User = require("./models/user");
 const validateSignUpData = require("./utils/validation");
 const bcrypt = require('bcrypt');
 const validator =  require('validator');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 
 app.use(express.json());
+app.use(cookieParser());
+
 
 app.post("/signup", async (req, res) => {
     try {
@@ -32,33 +36,54 @@ app.post("/signup", async (req, res) => {
 });
 
 
-app.post("/login", async (req,res)=>{
-  try {
-    const { email, password } = req.body;
+app.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
 
-    if(!validator.isEmail(email)){
-        return res.status(400).send("Error: Email is not valid");
-    }
-   const validUser = await User.findOne({ email: email });
-   if (!validUser) {
-    throw new Error("User not found");
-   }
-    const validCred = await bcrypt.compare(password, validUser.password);
+        if (!validator.isEmail(email)) {
+            return res.status(400).send("Error: Email is not valid");
+        }
 
-    if(validCred){
-        res.send("User logged in");
-    } else {
-        res.send("Invalid credentials");
+        const validUser = await User.findOne({ email: email });
+        if (!validUser) {
+            return res.status(404).send("User not found");
+        }
+
+        const validCred = await bcrypt.compare(password, validUser.password);
+        if (validCred) {
+            const token = await jwt.sign({_id:validUser._id},  "shrey999");
+
+            res.cookie("token", token);
+            res.send("User logged in");
+        } else {
+            res.status(401).send("Invalid credentials");
+        }
+    } catch (err) {
+        res.status(400).send("Error:" + err.message);
     }
-  } catch(err) {
-    res.status(400).send("Error:" + err.message);
-  }
+});
+
+app.get("/profile",  async (req,  res) => {
+    try {
+    const {token} = req.cookies;
+    if(!token){
+        return res.status(401).send("invalid token");
+    }
+    const decodedmsg = await jwt.verify(token, "shrey999");
+    const user = await User.findOne({_id: decodedmsg._id});
+    if(!user){
+        return res.status(404).send("user not found");
+    }
+ res.send("welcome "+ user.firstName);
+    } catch (err) {
+        res.status(400).send("Error:" + err.message);
+    }
 });
 
 
-app.patch("/feed", async (req, res) => {
+app.get("/feed", async (req, res) => {
     try {
-        const users = await User.updateMany({ email: "www.shreyas.com" });
+        const users = await User.findOne({});
         res.send(users);
     } catch (err) {
         res.send("something went wrong");
