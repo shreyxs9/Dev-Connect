73de0d37b61ca856dd839b0b1f4c5d08be33a420
Shreyas <106990933+shreyxs9@users.noreply.github.com>
2025-02-25 11:33:46 +0530

    Update database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDb = async () =>{
- await mongoose.connect("mongodb+srv://heartwise:shreyaskumaR999$@shreyas.nnpp8np.mongodb.net/dev-connect");
+ await mongoose.connect("mongodb+srv://heartwise:@shreyas.nnpp8np.mongodb.net/dev-connect");
 };
 
 module.exports = connectDb;
